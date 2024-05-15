7453a11590702393bf3fc54d92036f5464b1c8a1
Beatriz Araujo <beatriz.a.0702@gmail.com>
2024-05-15 17:49:27 -0300

    feat: soluciona case 1 e 2

---
+++ src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,42 @@
+package candidatura;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        // analisarCandidato(1999.0);
+        // analisarCandidato(2000.0);
+        // analisarCandidato(2200.0);
+        selecionarCandidatosComBaseNoSalario();
+    }
+
+    static void analisarCandidato(double expectativaSalarial){
+        double salarioBase = 2000.00;
+        if (salarioBase > expectativaSalarial) System.out.println("Ligar para o candidato");
+        else if (salarioBase == expectativaSalarial) System.out.println("Ligar para o candidato com contra proposta");
+        else System.out.println("Aguardando resultados dos demais candidatos");
+
+    }
+
+    static void selecionarCandidatosComBaseNoSalario(){
+        String[] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+        int candidatosSelecionados = 0;
+
+        double salarioBase = 2000.00;
+
+        for(int i = 0; candidatosSelecionados < 5 && i < candidatos.length; i++){
+            String candidato = candidatos[i];
+            double pretencaoSalarial = valorPretendido();
+
+            System.out.println("O candidado " + candidato + " tem uma pretenção salarial de " + pretencaoSalarial);
+            if(salarioBase >= pretencaoSalarial) {
+                System.out.println("Candidato selecionado");
+                candidatosSelecionados++;
+            }
+        }
+    }
+
+    static double valorPretendido() {
+     return ThreadLocalRandom.current().nextDouble(1800, 2200);
+}
+}
